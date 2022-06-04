5ac8b7fb2c634170b523c5ceeddcb2c5bba13002
ieatCookiieezz <54184284+ieatCookiieezz@users.noreply.github.com>
2022-06-04 21:18:16 +0200

    Add files via upload

---
+++ libertycrimelife-loadingscreen/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Kuehlschrank | Rouge.Designs'
+
+loadscreen_cursor 'yes'
+loadscreen 'ui/index.html'
+loadscreen_manual_shutdown 'yes'
+client_script 'client.lua'
+
+files {
+    'ui/index.html',
+    'ui/style.css',
+    'ui/script/config.js',
+    'ui/script/script.js'
+}
---
+++ libertycrimelife-loadingscreen/ui/script/config.js
@@ -0,0 +1,34 @@
+Config = []
+
+Config.Servername = "LibertyCrimeLife OneShot";
+
+Config.Welcomemessage = "Ingame Commands : /fps on /fps off /light on /light off ";
+
+//Logo link
+Config.HeaderImage = "https://cdn.discordapp.com/attachments/955510865061376090/964862108653211728/Logo.png";
+
+Config.Overlaybuttontext =  "Drücke um das Overlay An/Aus zu schalten.";
+
+//Lautstärke des Videos 1-100
+Config.SoundVolume = 4;
+
+//Youtube Video ID
+Config.YoutubeVideoID = "Y_5Ad2RUXH4";
+
+
+//NICHT MEHR ALS 8 LINKS
+Config.Socialmedias = [
+        //{btnname: "Discord", icon: "https://cdn.discordapp.com/attachments/955510865061376090/965211983697825822/discord.png", link: "https://discord.gg/libertycrimelife"},
+
+];
+
+Config.Members = [
+        {name: "Cookiieezz", rank: "Projektinhaber", media: "https://cdn.discordapp.com/attachments/964614986137743392/964853190749224970/animated.gif"},
+        {name: "Kevin", rank: "Management", media: "https://cdn.discordapp.com/attachments/964614986137743392/964853190749224970/animated.gif"},
+        {name: "Mohammad", rank: "Management", media: "https://cdn.discordapp.com/attachments/964614986137743392/964853190749224970/animated.gif"},
+        {name: "Arda", rank: "Management", media: "https://cdn.discordapp.com/attachments/964614986137743392/964853190749224970/animated.gif"},
+        {name: "Suhamedin", rank: "Teamleitung", media: "https://cdn.discordapp.com/attachments/964614986137743392/964853190749224970/animated.gif"},
+        {name: "Gabrijel", rank: "Teamleitung", media: "https://cdn.discordapp.com/attachments/964614986137743392/964853190749224970/animated.gif"},
+        {name: "Mohammad", rank: "Fraktionsleitung", media: "https://cdn.discordapp.com/attachments/964614986137743392/964853190749224970/animated.gif"},
+        {name: "Swanzy", rank: "Fraktionsleitung", media: "https://cdn.discordapp.com/attachments/964614986137743392/964853190749224970/animated.gif"},
+];
